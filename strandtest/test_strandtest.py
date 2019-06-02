f4a276a5bb5d0ed79f090542d17bbe03d4d15129
Benedikt Grether <info@benediktgrether.de>
2019-06-02 14:34:57 +0200

    Add Button to Programm Logic #8

--- strandtest/test_strandtest.py
+++ strandtest/test_strandtest.py
@@ -10,6 +10,8 @@ from neopixel import *
 import argparse
 from flask import Flask
 import random
+import RPi.GPIO as GPIO
+
 
 app = Flask(__name__)
 
@@ -24,9 +26,15 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+# Button Pin Configuration
+ButtonPin = 24
+# Set Led status to True(OFF)
+Led_status = True
+
+
 leds = {
-    'red': (0,255,0),
-    'blue': (0, 0 ,255),
+    'red': (0, 255, 0),
+    'blue': (0, 0, 255),
     'green': (255, 0, 0)
 }
 
@@ -36,6 +44,47 @@ usedColorStrip = {
 
 }
 
+print(ButtonPin)
+print(LED_PIN)
+
+#
+# Define a setup function for some setup
+
+
+def setup():
+	# Set the GPIO modes to BCM Numbering
+	GPIO.setmode(GPIO.BCM)
+	# Set LedPin's mode to output,
+	# and initial level to high (3.3v)
+	#GPIO.setup(LedPin, GPIO.OUT, initial=GPIO.HIGH)
+	# Set BtnPin's mode to input,
+	# and pull up to high (3.3V)
+	GPIO.setup(ButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	# Set up a falling detect on BtnPin,
+	# and callback function to swLed
+	GPIO.add_event_detect(ButtonPin, GPIO.FALLING, callback=swLed)
+
+# Define a callback function for button callback
+
+
+def swLed(ev=None):
+    global Led_status
+    # Switch led status(on-->off; off-->on)
+    #Led_status = not Led_status
+    #GPIO.output(Led_status)
+    if Led_status:
+        print('LED OFF')
+        randomG = random.randint(0, 255)
+        randomR = random.randint(0, 255)
+        randomB = random.randint(0, 255)
+        #print(randomColor)
+        print("userColor?")
+        print(userColor)
+        setRandomColor(strip, Color(randomG, randomR, randomB))
+        time.sleep(10)
+    else:
+        print('LED ON')
+#
 
 
 # Define functions which animate LEDs in various ways.
@@ -64,7 +113,7 @@ def setRandomColor(strip, color, wait_ms=50):
             strip.setPixelColor((j - k), userColor[0])
             strip.show()
             time.sleep(wait_ms/1000.0)
-    usedColorStrip.update({randomInt : color}) 
+    usedColorStrip.update({randomInt: color})
     for keys, values in usedColorStrip.items():
         strip.setPixelColor(keys, values)
         strip.show()
@@ -72,16 +121,17 @@ def setRandomColor(strip, color, wait_ms=50):
 
 @app.route("/led/<color>/<state>")
 def set_led(color, state):
-    for webColor in leds.keys():   
+    for webColor in leds.keys():
         if color == webColor:
             if state == 'on':
-    #           GPIO.output(leds[color], 1)
-                colorWipe(strip, Color(leds[webColor][0], leds[webColor][1], leds[webColor][2]))  # Red wipe
+		    #           GPIO.output(leds[color], 1)
+                colorWipe(strip, Color(
+                    leds[webColor][0], leds[webColor][1], leds[webColor][2]))  # Red wipe
                 print("randome Color?")
                 print(userColor)
                 return 'LED On: {}'.format(color)
             else:
-    #           GPIO.output(leds[color], 0)
+		    #           GPIO.output(leds[color], 0)
                 colorWipe(strip, Color(0, 0, 0))
                 print("if randome color click? im here?")
                 print(userColor)
@@ -91,14 +141,13 @@ def set_led(color, state):
             randomG = random.randint(0, 255)
             randomR = random.randint(0, 255)
             randomB = random.randint(0, 255)
-            randomColor = (randomG, randomR, randomB)
             #print(randomColor)
             print("userColor?")
             print(userColor)
             setRandomColor(strip, Color(randomG, randomR, randomB))
 
-
             return 'Invalid LED: {}'.format(color)
+
 
 app.add_url_rule("/", "index", lambda: 'Hello World!')
 # Main program logic follows:
@@ -106,16 +155,18 @@ if __name__ == '__main__':
 
     # Process arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    parser.add_argument('-c', '--clear', action='store_true',
+                        help='clear the display on exit')
     args = parser.parse_args()
 
     # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip = Adafruit_NeoPixel(
+    	LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
     strip.begin()
+    setup()
     app.run(host='0.0.0.0')
 
-
-    print ('Press Ctrl-C to quit.')
+    print('Press Ctrl-C to quit.')
     if not args.clear:
         print('Use "-c" argument to clear LEDs on exit')
